ac505f528cb398257ac09a44c8995ad78c800586
Dan Raymond <danraymond95@gmail.com>
2020-01-25 13:29:35 +0200

    deploy to heroku

--- first-app/config/keys.js
+++ first-app/config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://127.0.0.1:27017/'
+    mongoURI: 'mongodb+srv://missions:missions123@cluster0-cxhwg.mongodb.net/test?retryWrites=true&w=majority'
 }
+
+//'mongodb://127.0.0.1:27017/'
